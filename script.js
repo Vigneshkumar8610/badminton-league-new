574fe83c75ac121f8635dff31d366122087cbd4d
Vigneshkumar8610 <vigneshkumar22005@gmail.com>
2025-05-23 00:43:45 +0530

    badminton page created

---
+++ script.js
@@ -0,0 +1,65 @@
+
+  let players = JSON.parse(localStorage.getItem("players")) || [];
+
+    
+  window.onload = () => {
+    renderPlayerList();
+  };
+
+  function renderPlayerList() {
+    const listElement = document.getElementById("playerList");
+    listElement.innerHTML = ""; 
+
+    players.forEach((name) => {
+      const li = document.createElement("li");
+      li.textContent = `${name}`;
+      listElement.appendChild(li);
+    });
+  }
+
+  function addPlayer() {
+    const nameInput = document.getElementById("playerNameInput");
+    const playerName = nameInput.value.trim();
+
+    if (playerName === "") {
+      alert("Please enter a name.");
+      return;
+    }
+
+
+    players.push(playerName);
+
+
+    localStorage.setItem("players", JSON.stringify(players));
+
+    nameInput.value = "";
+    renderPlayerList();
+  }
+
+  function removePlayer(){
+    const nameIp = document.getElementById("playerNameIp");
+    const removeName = nameIp.value;
+    players.splice(removeName,1);
+    renderPlayerList();
+  }
+  
+
+
+
+
+
+
+
+
+
+
+//   MATCH TRACKING
+console.log(JSON.stringify(players));
+
+
+const result = "";
+
+let player1, player2;
+
+player1 = "";
+
