22e4d2f19f1741137a3a4082aff2d12ec39827e9
Vigneshkumar8610 <vigneshkumar22005@gmail.com>
2025-05-23 11:13:01 +0530

    badminton page created

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 
-  let players = JSON.parse(localStorage.getItem("players")) || [];
+let players = JSON.parse(localStorage.getItem("players")) || [];
 
-    
-  window.onload = () => {
-    renderPlayerList();
-  };
 
-  function renderPlayerList() {
-    const listElement = document.getElementById("playerList");
-    listElement.innerHTML = ""; 
+window.onload = () => {
+  renderPlayerList();
+};
 
-    players.forEach((name) => {
-      const li = document.createElement("li");
-      li.textContent = `${name}`;
-      listElement.appendChild(li);
-    });
+function renderPlayerList() {
+  const listElement = document.getElementById("playerList");
+  listElement.innerHTML = "";
+
+  players.forEach((name) => {
+    const li = document.createElement("li");
+    li.textContent = `${name}`;
+    listElement.appendChild(li);
+  });
+}
+
+function addPlayer() {
+  const nameInput = document.getElementById("playerNameInput");
+  const playerName = nameInput.value.trim();
+
+  if (playerName === "") {
+    alert("Please enter a name.");
+    return;
   }
 
-  function addPlayer() {
-    const nameInput = document.getElementById("playerNameInput");
-    const playerName = nameInput.value.trim();
 
-    if (playerName === "") {
-      alert("Please enter a name.");
-      return;
-    }
+  players.push(playerName);
 
 
-    players.push(playerName);
+  localStorage.setItem("players", JSON.stringify(players));
 
+  nameInput.value = "";
+  renderPlayerList();
+}
 
-    localStorage.setItem("players", JSON.stringify(players));
-
-    nameInput.value = "";
-    renderPlayerList();
-  }
-
-  function removePlayer(){
-    const nameIp = document.getElementById("playerNameIp");
-    const removeName = nameIp.value;
-    players.splice(removeName,1);
-    renderPlayerList();
-  }
-  
-
-
-
-
-
-
-
-
-
+function removePlayer() {
+  const nameIp = document.getElementById("playerNameIp");
+  const removeName = nameIp.value;
+  players.splice(removeName, 1);
+  renderPlayerList();
+}
 
 //   MATCH TRACKING
 console.log(JSON.stringify(players));
